66284d9cba9164de0f1e761bc80ffc33c060a3a5
Michael Einsiedler <michael.einsiedler@softaware.at>
2021-04-14 17:39:47 +0200

    update unit tests for new SerialPortStream version and new failing behavior

--- SerialPortStreamTest.Console/Program.cs
+++ SerialPortStreamTest.Console/Program.cs
@@ -10,33 +10,34 @@ namespace SerialPortStreamTest
         {
             Console.WriteLine("Start");
 
-            using (var serialPortStream = new SerialPortStream("COM1", 9600, 8, Parity.None, StopBits.One))
+            using (var serialPortStreamWrite = new SerialPortStream("COM1", 9600, 8, Parity.None, StopBits.One))
+            using (var serialPortStreamRead = new SerialPortStream("COM2", 9600, 8, Parity.None, StopBits.One))
             {
-                serialPortStream.Open();
+                serialPortStreamWrite.Open();
+                serialPortStreamRead.Open();
 
                 Console.WriteLine("Serial Port Opened");
 
                 var buffer = new byte[1024];
-                var readTask = Task.Run(async () => await serialPortStream.ReadAsync(buffer, 0, buffer.Length));
+                var readTask = Task.Run(async () => await serialPortStreamRead.ReadAsync(buffer, 0, buffer.Length));
 
                 Console.WriteLine("Wait for write. Enter any key.");
                 Console.ReadKey();
                 
 
-                await Task.Run(async () =>
-                {
-                    var bytes = new byte[] { 0x01, 0x02, 0x03 };
+                var bytes = new byte[] { 0x01, 0x02, 0x03 };
 
-                    Console.WriteLine("Write started");
-                    await serialPortStream.WriteAsync(bytes, 0, bytes.Length);
-                    await serialPortStream.FlushAsync();
-                    Console.WriteLine("Write finished");
-                });
+                Console.WriteLine("Write started");
+                await serialPortStreamWrite.WriteAsync(bytes, 0, bytes.Length);
+                await serialPortStreamWrite.FlushAsync();
+                Console.WriteLine("Write finished");
+
 
                 Console.WriteLine("Awaiting read...");
 
                 await readTask;
-                
+
+                Console.WriteLine($"Buffer: {buffer[0]}, {buffer[1]}, {buffer[2]}");
 
                 Console.WriteLine("Finished.");
             }
--- SerialPortStreamTest.Tests/SerialPortStreamTest.cs
+++ SerialPortStreamTest.Tests/SerialPortStreamTest.cs
@@ -7,40 +7,30 @@ namespace SerialPortStreamTest.Tests
     public class SerialPortStreamTest
     {
         [Test]
-        [Ignore("When we don't ignore this test (which runs first), the second test works even with .NET Core 3.1 too.")]
         [Order(1)]
+        [Timeout(2000)] // We abort the test after timeout. This tests the blocking behavior in ReadAsync and the test will fail if ReadAsync blocks.
         public async Task WriteAsync()
         {
-            using (var serialPortStream = new SerialPortStream("COM1", 9600, 8, Parity.None, StopBits.One))
+            using (var serialPortStreamWrite = new SerialPortStream("COM1", 9600, 8, Parity.None, StopBits.One))
+            using (var serialPortStreamRead = new SerialPortStream("COM2", 9600, 8, Parity.None, StopBits.One))
             {
-                serialPortStream.Open();
-
-                var bytes = new byte[] { 0x01, 0x02, 0x03 };
-
-                await serialPortStream.WriteAsync(bytes, 0, bytes.Length);
-                await serialPortStream.FlushAsync();
-            }
-        }
-
-        [Test]
-        [Order(2)]
-        [Timeout(2000)] // We abort the test after timeout so that we can "test" the waiting time in WriteAsync.
-        public async Task ReadAndWriteInDifferntThreadsAtSameTime()
-        {
-            using (var serialPortStream = new SerialPortStream("COM1", 9600, 8, Parity.None, StopBits.One))
-            {
-                serialPortStream.Open();
+                serialPortStreamWrite.Open();
+                serialPortStreamRead.Open();
 
                 var buffer = new byte[1024];
-                var readTask = Task.Run(async () => await serialPortStream.ReadAsync(buffer, 0, buffer.Length));
+                var readTask = Task.Run(async () => await serialPortStreamRead.ReadAsync(buffer, 0, buffer.Length));
 
-                await Task.Run(async () =>
-                {
-                    var bytes = new byte[] { 0x01, 0x02, 0x03 };
+                var bytes = new byte[] { 0x01, 0x02, 0x03 };
+                await serialPortStreamWrite.WriteAsync(bytes, 0, bytes.Length);
+                await serialPortStreamWrite.FlushAsync();
 
-                    await serialPortStream.WriteAsync(bytes, 0, bytes.Length); // Call blocks here with .NET Core 3.1.
-                    await serialPortStream.FlushAsync();
-                });
+                // ReadAsync blocks here even if something gets written and flushed to the underlying COM device.
+                // Fails for netcoreapp3.1, works with net472
+                await readTask;
+
+                Assert.That(buffer[0], Is.EqualTo(0x01));
+                Assert.That(buffer[1], Is.EqualTo(0x02));
+                Assert.That(buffer[2], Is.EqualTo(0x03));
             }
         }
     }
